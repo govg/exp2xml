e275fe92611c4a7636c7fd658eb6c7dc55ebecf9
asnani04 <asnani04@gmail.com>
2017-04-06 16:56:38 +0000

    testing rf_ts

--- Tests/running.py
+++ Tests/running.py
@@ -2,16 +2,16 @@
 import sys
 sys.path.append("../Python/")
 
-from rf_ts_level import RandomForest
+from rf_ts import RandomForest
 import numpy as np
 
 model = RandomForest(numTrees=20, maxDepth=10, splitCriterion='exp')
-Xtr = np.load("../Data/spam/Xtrain.npy")
+Xtr = np.load("../Data/mnist/Xtrain.npy")
 print(Xtr.shape)
-Xte = np.load("../Data/spam/Xtest.npy")
-Ytr = np.load("../Data/spam/Ytrain.npy")
+Xte = np.load("../Data/mnist/Xtest.npy")
+Ytr = np.load("../Data/mnist/Ytrain.npy")
 print(Ytr.shape)
-Yte = np.load("../Data/spam/Ytest.npy")
+Yte = np.load("../Data/mnist/Ytest.npy")
 
 model.fit(Xtr, Ytr)
 
